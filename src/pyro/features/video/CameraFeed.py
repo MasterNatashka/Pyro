8461fe6ef11953dfc542e43bb8ea727ad886d8f1
Natalia Silivestrova <masternatashka@gmail.com>
2025-07-07 21:05:13 +0300

    добавила управление PTZ

--- src/pyro/features/video/CameraFeed.py
+++ src/pyro/features/video/CameraFeed.py
@@ -1,29 +1,75 @@
 import cv2
 import os
+import requests
+from requests.auth import HTTPDigestAuth
 
 
 class CameraFeed:
     def __init__(self, source):
-        self.source = source
-        self.cap = cv2.VideoCapture(self.rtsp_url())
+        self._source = source
+        self._cap = cv2.VideoCapture(self._rtsp_url())
+        self._ptz_url =  f"http://{source}:8899/onvif/ptz_service"
+        self._ptz_headers = {'Content-Type': 'application/soap+xml'}
+        self._ptz_auth = HTTPDigestAuth(os.environ.get("RTSP_USER"), os.environ.get("RTSP_PASSWORD"))
+        self._ptz_timeout = 5
 
     def __del__(self):
-        if hasattr(self, "cap"):
-            self.cap.release()
+        self._cap.release()
 
-    def rtsp_url(self):
-        if self.source == os.environ.get("TEST_CAM_ADDR"):
+    def stream(self):
+        while True:
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + self._get_frame() + b'\r\n')
+
+    def ptz_move(self, pan=0, tilt=0):
+        # TODO проверка на крайние положения
+        return requests.post(
+            self._ptz_url,
+            headers=self._ptz_headers,
+            auth=self._ptz_auth,
+            timeout=self._ptz_timeout,
+            data=f"""
+                <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+                    <soap:Body>
+                        <tptz:ContinuousMove xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl">
+                            <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+                            <tptz:Velocity>
+                                <tt:PanTilt x={pan} y={tilt} xmlns:tt="http://www.onvif.org/ver10/schema"/>
+                            </tptz:Velocity>
+                        </tptz:ContinuousMove>
+                    </soap:Body>
+                </soap:Envelope>
+            """
+        )
+
+    def ptz_stop(self):
+        return requests.post(
+            self._ptz_url,
+            headers=self._ptz_headers,
+            auth=self._ptz_auth,
+            timeout=self._ptz_timeout,
+            data=f"""
+                <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+                    <soap:Body>
+                        <tptz:Stop xmlns:tptz="http://www.onvif.org/ver20/ptz/wsdl">
+                            <tptz:ProfileToken>Profile_1</tptz:ProfileToken>
+                            <tptz:PanTilt>true</tptz:PanTilt>
+                            <tptz:Zoom>true</tptz:Zoom>
+                        </tptz:Stop>
+                    </soap:Body>
+                </soap:Envelope>
+            """
+        )
+
+    def _rtsp_url(self):
+        if self._source == os.environ.get("TEST_CAM_ADDR"):
             return os.environ.get("TEST_CAM_RTSP")
         user = os.environ.get("RTSP_USER")
         password = os.environ.get("RTSP_PASSWORD")
-        return f"rtsp://{self.source}:554/user={user}&password={password}&channel=0&stream=1?.sdp"
+        return f"rtsp://{self._source}:554/user={user}&password={password}&channel=0&stream=1?.sdp"
 
-    def ptz_url(self):
-        return f"http://{self.source}:8899/onvif/ptz_service"
-
-    def get_frame(self):
+    def _get_frame(self):
         try:
-            ok, frame = self.cap.read()
+            ok, frame = self._cap.read()
             if not ok:
                 return None
             ok, buf = cv2.imencode('.jpg', frame)
@@ -32,6 +78,3 @@ class CameraFeed:
             print(f"Ошибка получения кадра: {e}")
             return None
 
-    def stream(self):
-        while True:
-            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + self.get_frame() + b'\r\n')
--- src/pyro/views.py
+++ src/pyro/views.py
@@ -1,5 +1,6 @@
-from django.http import StreamingHttpResponse
+from django.http import StreamingHttpResponse, HttpResponse
 from django.shortcuts import render
+import json
 from .models import Camera, Location
 from .features.video.CameraFeed import CameraFeed
 
@@ -24,7 +25,26 @@ def settings(request):
 def video(request, id):
     cam = Camera.objects.get(pk=id)
     vid = CameraFeed(cam.address)
-    return StreamingHttpResponse(
-        vid.stream(),
-        content_type='multipart/x-mixed-replace; boundary=frame'
-    )
+    if request.method == 'GET':
+        return StreamingHttpResponse(
+            vid.stream(),
+            content_type='multipart/x-mixed-replace; boundary=frame'
+        )
+    elif request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            action = data.get('action')
+            if action == 'pan-tilt':
+                pan = data.get('pan')
+                tilt = data.get('tilt')
+                vid.ptz_move(pan=pan, tilt=tilt)
+            # elif action == 'zoom':
+            #     zoom = data.get('zoom')
+            #     vid.ptz_zoom(zoom)
+            elif action == 'stop':
+                vid.ptz_stop()
+            return HttpResponse(status=200)
+        except Exception as err:
+            return HttpResponseBadRequest(str(err))
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
